13b9aab2bf16196d7aed7adfb54098c89a17df7b
Ivan Kolganov <imkolganov@gmail.com>
2025-04-15 12:38:51 +0300

    added models for telegram bot

--- OpenVPNGateMonitor.DataBase/ConfigurationModels/SettingConfiguration.cs
+++ OpenVPNGateMonitor.DataBase/ConfigurationModels/SettingConfiguration.cs
@@ -1,5 +1,4 @@
 ﻿using OpenVPNGateMonitor.Models;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using OpenVPNGateMonitor.DataBase.ConfigurationModels.Seeds;
 
--- OpenVPNGateMonitor.DataBase/Contexts/ApplicationDbContext.cs
+++ OpenVPNGateMonitor.DataBase/Contexts/ApplicationDbContext.cs
@@ -36,6 +36,7 @@ public class ApplicationDbContext : DbContext
     public DbSet<OpenVpnServerOvpnFileConfig> OpenVpnServerOvpnFileConfigs { get; set; } = null!;
     public DbSet<ClientApplication> ClientApplications { get; set; } = null!;
     public DbSet<Setting> Settings { get; set; } = null!;
+    public DbSet<TelegramBotUser> TelegramBotUsers { get; set; } = null!;
     
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
@@ -49,6 +50,7 @@ public class ApplicationDbContext : DbContext
         modelBuilder.ApplyConfiguration(new OpenVpnServerOvpnFileConfigConfiguration());
         modelBuilder.ApplyConfiguration(new ClientApplicationConfiguration());
         modelBuilder.ApplyConfiguration(new SettingConfiguration());
+        modelBuilder.ApplyConfiguration(new TelegramBotUserConfiguration());
     }
     
     private void UpdateTimestamps()
