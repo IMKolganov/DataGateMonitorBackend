7aa63efc8378a53031d0413df9ccda330587de4a
Ivan Kolganov <imkolganov@gmail.com>
2025-04-17 19:04:42 +0300

    fixed routes

--- OpenVPNGateMonitor.SharedModels/TelegramBotLocalization/Requests/IsExistTelegramUserLanguagePreferenceRequest.cs
+++ OpenVPNGateMonitor.SharedModels/TelegramBotLocalization/Requests/IsExistTelegramUserLanguagePreferenceRequest.cs
@@ -1,9 +1,11 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
 
 namespace OpenVPNGateMonitor.SharedModels.TelegramBotLocalization.Requests;
 
 public class IsExistTelegramUserLanguagePreferenceRequest
 {
+    [FromRoute(Name = "telegramId")]
     [Required(ErrorMessage = "TelegramId is required.")]
     public long TelegramId { get; set; }
 }
