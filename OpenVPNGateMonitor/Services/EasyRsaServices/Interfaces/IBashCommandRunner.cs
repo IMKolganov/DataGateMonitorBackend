cc4aff6f43821747ba47a4b1b5c45fdeafa80d7b
Ivan Kolganov <imkolganov@gmail.com>
2025-04-22 17:38:57 +0300

    Refactors OpenVPN client status retrieval
    Improves logging and adds debug information for connected clients.
    This helps in debugging and monitoring client connections.
    
    Removes the EasyRSA-related services as they are no longer needed.

--- OpenVPNGateMonitor/Services/EasyRsaServices/Interfaces/IBashCommandRunner.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace OpenVPNGateMonitor.Services.EasyRsaServices.Interfaces;
-
-public interface IBashCommandRunner
-{
-    (string Output, string Error, int ExitCode) RunCommand(string command);
-}
--- OpenVPNGateMonitor/Services/EasyRsaServices/Interfaces/IEasyRsaParseDbService.cs
+++
@@ -1,8 +0,0 @@
-﻿using OpenVPNGateMonitor.Models.Helpers;
-
-namespace OpenVPNGateMonitor.Services.EasyRsaServices.Interfaces;
-
-public interface IEasyRsaParseDbService
-{
-    List<CertificateCaInfo> ParseCertificateInfoInIndexFile(string pkiPath);
-}
