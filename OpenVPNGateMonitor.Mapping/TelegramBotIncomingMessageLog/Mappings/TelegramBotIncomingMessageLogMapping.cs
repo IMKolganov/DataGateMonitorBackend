f74768947fdde27cf22ee32a7b15c826f67d9744
Ivan Kolganov <imkolganov@gmail.com>
2025-06-11 00:41:35 +0300

    Adds Telegram bot message logging
    Adds functionality to log incoming Telegram bot messages.
    
    Creates a new mapping profile for Telegram bot incoming messages
    and updates shared models to include default timestamps.
    Increments the version of the shared models package.

---
+++ OpenVPNGateMonitor.Mapping/TelegramBotIncomingMessageLog/Mappings/TelegramBotIncomingMessageLogMapping.cs
@@ -0,0 +1,19 @@
+﻿using Mapster;
+using OpenVPNGateMonitor.SharedModels.DataGateMonitorBackend.TelegramBotIncomingMessageLog.Dto;
+
+namespace OpenVPNGateMonitor.Mapping.TelegramBotIncomingMessageLog.Mappings;
+
+public class TelegramBotIncomingMessageLogMapping : IRegister
+{
+    public void Register(TypeAdapterConfig config)
+    {
+        TypeAdapterConfig<MessageDto, Models.IncomingMessageLog>
+            .NewConfig()
+            .Ignore(dest => dest.Id);
+           
+        TypeAdapterConfig<Models.IncomingMessageLog, MessageDto>
+            .NewConfig()
+            .Map(dest => dest.CreateDate, src => DateTime.UtcNow)
+            .Map(dest => dest.LastUpdate, src => DateTime.UtcNow);
+    }
+}
