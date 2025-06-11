107a62a9130d0d6e5cc3e94c2c3ab6435fab061e
Ivan Kolganov <imkolganov@gmail.com>
2025-06-11 18:23:39 +0300

    Adds OpenVPN server event logging
    Implements event logging for OpenVPN servers.
    Creates DTOs, requests, and responses for retrieving event logs.
    Adds an endpoint to fetch events by VPN server ID with pagination.
    
    Updates SharedModels package version to 1.0.29.

---
+++ OpenVPNGateMonitor.SharedModels/DataGateMonitorBackend/OpenVpnServerEvent/Responses/VpnServerEventResponse.cs
@@ -0,0 +1,9 @@
+﻿using OpenVPNGateMonitor.SharedModels.DataGateMonitorBackend.OpenVpnServerEvent.Dto;
+
+namespace OpenVPNGateMonitor.SharedModels.DataGateMonitorBackend.OpenVpnServerEvent.Responses;
+
+public class VpnServerEventResponse
+{
+    public int TotalCount { get; set; }
+    public List<OpenVpnServerEventLogDto> Events { get; set; } = new();
+}
