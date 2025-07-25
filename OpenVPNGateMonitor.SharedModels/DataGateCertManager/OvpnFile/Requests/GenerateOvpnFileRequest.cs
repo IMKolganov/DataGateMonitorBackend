317a0e3771504cbf5e7da6731a127457ec9ecf1e
Ivan Kolganov <imkolganov@gmail.com>
2025-07-25 05:01:58 +0300

    Escapes the friendly name in OVPN config template
    The change adds quotes around the `{{friendly_name}}` variable in the OpenVPN configuration template.
    This ensures that the friendly name is properly escaped and handled by the OpenVPN client, especially when the name contains spaces or special characters.
    A new migration is also added that shields/applies the new config templates.

--- OpenVPNGateMonitor.SharedModels/DataGateCertManager/OvpnFile/Requests/GenerateOvpnFileRequest.cs
+++ OpenVPNGateMonitor.SharedModels/DataGateCertManager/OvpnFile/Requests/GenerateOvpnFileRequest.cs
@@ -19,7 +19,7 @@ public class GenerateOvpnFileRequest
     [DefaultValue(1194)]
     public int ServerPort { get; set; } = 1194;
 
-    [DefaultValue("setenv FRIENDLY_NAME {{friendly_name}}\nclient\ndev tun\nproto udp\nremote {{server_ip}} {{server_port}}\nresolv-retry infinite\nnobind" +
+    [DefaultValue("setenv FRIENDLY_NAME \"{{friendly_name}}\"\nclient\ndev tun\nproto udp\nremote {{server_ip}} {{server_port}}\nresolv-retry infinite\nnobind" +
                   "\nremote-cert-tls server\ntls-version-min 1.2\ncipher AES-256-CBC\nauth SHA256\nauth-nocache\nverb 3" +
                   "\n<ca>\n{{ca_cert}}\n</ca>\n<cert>\n{{client_cert}}\n</cert>\n<key>\n{{client_key}}\n</key>\n<tls-crypt>" +
                   "\n{{tls_auth_key}}\n</tls-crypt>")]
