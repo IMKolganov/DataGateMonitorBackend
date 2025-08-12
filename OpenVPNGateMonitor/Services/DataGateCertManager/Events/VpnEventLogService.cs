36a468b44be055ddf1c7a6068490fc6840625f81
Ivan Kolganov <imkolganov@gmail.com>
2025-08-12 18:56:29 +0300

    Adds command service and improves query services
    Implements generic command service for performing database operations.
    Introduces `AddRangeAsync` to the repository interface and implementation
    for efficient bulk insertion.
    
    Enhances query services by introducing an endpoint for paginated retrieval of
    event logs filtered by VPN server ID.
    Adds `PagedResult` class to hold pagination information.
    
    Refactors VPN event log service to utilize the new query service endpoint,
    improving efficiency and data retrieval capabilities.

--- OpenVPNGateMonitor/Services/DataGateCertManager/Events/VpnEventLogService.cs
+++ OpenVPNGateMonitor/Services/DataGateCertManager/Events/VpnEventLogService.cs
@@ -1,13 +1,13 @@
 ﻿using Mapster;
-using Microsoft.EntityFrameworkCore;
-using OpenVPNGateMonitor.DataBase.UnitOfWork;
+using OpenVPNGateMonitor.DataBase.Services.Query.OpenVpnServerEventLogTable;
 using OpenVPNGateMonitor.Models;
 using OpenVPNGateMonitor.SharedModels.DataGateMonitorBackend.OpenVpnServerEvent.Dto;
 using OpenVPNGateMonitor.SharedModels.DataGateMonitorBackend.OpenVpnServerEvent.Responses;
 
 namespace OpenVPNGateMonitor.Services.DataGateCertManager.Events;
 
-public class VpnEventLogService : IVpnEventLogService
+public class VpnEventLogService(
+    IOpenVpnServerEventLogQueryService openVpnServerEventLogQueryService) : IVpnEventLogService
 {
     public async Task SaveEventAsync(int vpnServerId, string eventType, OpenVpnServerEventLog data, string rawJson,
         CancellationToken cancellationToken)
@@ -32,22 +32,13 @@ public class VpnEventLogService : IVpnEventLogService
     public async Task<VpnServerEventResponse> GetEventByVpnServerIdAsync(
         int vpnServerId, int page, int pageSize, CancellationToken cancellationToken)
     {
-        var query = unitOfWork.GetQuery<OpenVpnServerEventLog>()
-            .AsQueryable()
-            .Where(x => x.VpnServerId == vpnServerId);
-
-        var totalCount = await query.CountAsync(cancellationToken);
-
-        var events = await query
-            .OrderByDescending(x => x.Id)
-            .Skip((page - 1) * pageSize)
-            .Take(pageSize)
-            .ToListAsync(cancellationToken);
+        var pageResult = await openVpnServerEventLogQueryService
+            .GetByVpnServerIdAsync(vpnServerId, page, pageSize, cancellationToken);
 
         return new VpnServerEventResponse
         {
-            TotalCount = totalCount,
-            Events = events.Adapt<List<OpenVpnServerEventLogDto>>()
+            TotalCount = pageResult.TotalCount,
+            Events = pageResult.Items.Adapt<List<OpenVpnServerEventLogDto>>()
         };
     }
 }
