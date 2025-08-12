66e3326df434ee3712738230c64cdcd5a9a94245
Ivan Kolganov <imkolganov@gmail.com>
2025-08-12 04:04:29 +0300

    Implements OpenVPN server overview query
    Adds a new query service to retrieve OpenVPN server overview data, including connected clients, session counts, and status logs.
    
    This allows for efficient retrieval of server status information with correlated subqueries, minimizing database round trips. Also enriches client data with Telegram user information if available.

---
+++ OpenVPNGateMonitor/Configurations/QueryConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using OpenVPNGateMonitor.DataBase.Services.Query;
+using OpenVPNGateMonitor.DataBase.Services.Query.OpenVpnServerTable;
+
+namespace OpenVPNGateMonitor.Configurations;
+
+public static class QueryConfiguration
+{
+    public static void ConfigureQuery(this IServiceCollection services)
+    {
+        services.AddScoped(typeof(IQueryService<,>), typeof(EfQueryService<,>));
+        services.AddScoped<IOpenVpnOverviewQuery, OpenVpnOverviewQuery>();
+    }
+}
