d152f93dd173f212c234f6a1d167d0e89fbbdc09
Иван Колганов <110064602+IMKolganov@users.noreply.github.com>
2025-10-18 00:02:18 +0300

    Adds application notification system
    Introduces a notification system to inform administrators about various events within the application.
    
    This includes notifications for:
    - System exceptions
    - File creations
    - Certificate issuances
    - Server status changes (up/down)
    
    The system utilizes a facade pattern for simplified usage and allows for routing notifications to different channels (e.g., web, Telegram).  The global exception handler is updated to leverage this new notification system.

---
+++ OpenVPNGateMonitor/Configurations/NotificationConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using OpenVPNGateMonitor.Services.Others.Notifications;
+
+namespace OpenVPNGateMonitor.Configurations;
+
+public static class NotificationConfiguration
+{
+    public static void ConfigureNotificationServices(this IServiceCollection services)
+    {
+        services.AddSingleton<INotificationCatalog, NotificationCatalog>();
+        services.AddScoped<IAppNotificationFacade, AppNotificationFacade>();
+    }
+}
