ad9c7814a4e28ce5e6ff7e399630600c7ffbf182
Ivan <imkolganov@gmail.com>
2025-10-19 21:03:41 +0300

    Adds Telegram message request models
    Adds request models for retrieving Telegram bot incoming messages.
    
    These models facilitate fetching messages by Telegram ID and by unique message ID,
    ensuring proper data validation and routing within the application.

---
+++ OpenVPNGateMonitor.SharedModels/DataGateMonitorBackend/TelegramBotIncomingMessageLog/Requests/GetAllByTelegramIdMessagesRequest.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OpenVPNGateMonitor.SharedModels.DataGateMonitorBackend.TelegramBotIncomingMessageLog.Requests;
+
+public class GetAllByTelegramIdMessagesRequest
+{
+    [Required(ErrorMessage = "telegramId is required.")]
+    [Range(1, int.MaxValue, ErrorMessage = "telegramId must be greater than 0.")]
+    [FromRoute(Name = "telegramId")]
+    public long TelegramId { get; set; }
+    
+}
