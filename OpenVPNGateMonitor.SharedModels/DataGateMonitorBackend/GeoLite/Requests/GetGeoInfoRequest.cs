e5b194118908ea7746e0f4c97055b2bfe4fb60a6
Ivan <imkolganov@gmail.com>
2025-10-24 23:26:23 +0300

    Refactors GeoLite models to shared project
    Moves GeoLite related models to the shared project for better code organization and reusability across different parts of the application.
    
    This change includes renaming and moving DTOs, requests, and responses
    to the OpenVPNGateMonitor.SharedModels project. It also updates the
    project's version number.

---
+++ OpenVPNGateMonitor.SharedModels/DataGateMonitorBackend/GeoLite/Requests/GetGeoInfoRequest.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OpenVPNGateMonitor.SharedModels.DataGateMonitorBackend.GeoLite.Requests;
+
+public class GetGeoInfoRequest
+{
+    [Required(ErrorMessage = "IpAddress is required.")]
+    public int IpAddress { get; set; }
+}
---
+++ OpenVPNGateMonitor.SharedModels/DataGateMonitorBackend/GeoLite/Responses/GetVersionDatabaseResponse.cs
@@ -0,0 +1,6 @@
+﻿namespace OpenVPNGateMonitor.SharedModels.DataGateMonitorBackend.GeoLite.Responses;
+
+public class GetVersionDatabaseResponse
+{
+    public string  DatabaseVersion  { get; set; } = string.Empty;
+}
