4ee7185ca3efced6945d04b9b7b9ce773a836724
Ivan <imkolganov@gmail.com>
2025-10-28 20:04:57 +0200

    Adds User management models
    Adds DTOs for managing user information.
    
    Creates response models for fetching user data.
    Includes a new controller for user-related API endpoints, initially secured with authorization.

---
+++ OpenVPNGateMonitor.SharedModels/DataGateMonitorBackend/User/Responses/UserResponse.cs
@@ -0,0 +1,8 @@
+﻿using OpenVPNGateMonitor.SharedModels.DataGateMonitorBackend.User.Responses.Dto;
+
+namespace OpenVPNGateMonitor.SharedModels.DataGateMonitorBackend.User.Responses;
+
+public class UsersResponse
+{
+    public UserDto User { get; set; } = new();
+}
