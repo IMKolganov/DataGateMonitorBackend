b3a1e73fe17180ce82a78bbf523efcbda21decf4
Ivan <imkolganov@gmail.com>
2025-11-03 01:57:29 +0200

    fixed mappings

---
+++ OpenVPNGateMonitor.Mapping/OpenVpnServerClients/Mappings/VpnServerClientMapping.cs
@@ -0,0 +1,17 @@
+﻿using Mapster;
+using OpenVPNGateMonitor.Models.Helpers.Services;
+using OpenVPNGateMonitor.SharedModels.DataGateMonitorBackend.OpenVpnServerClients.Responses;
+
+namespace OpenVPNGateMonitor.Mapping.OpenVpnServerClients.Mappings;
+
+public class VpnServerClientMapping : IRegister
+{
+    public void Register(TypeAdapterConfig config)
+    {
+        #region "get-all-connected"
+        config.NewConfig<VpnClientInfoResponseList, ConnectedClientsResponse>()
+            .Map(d => d.TotalCount, s => s.TotalCount)
+            .Map(d => d.Clients,    s => s.VpnClientInfoResponse);
+        #endregion
+    }
+}
