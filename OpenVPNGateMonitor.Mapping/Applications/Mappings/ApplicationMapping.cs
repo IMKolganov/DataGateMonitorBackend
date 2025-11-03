eaef853430f3a5625cb168e58d3fee351c962345
Ivan <imkolganov@gmail.com>
2025-11-03 23:35:35 +0200

    fixed mappings for ClientApplications

--- OpenVPNGateMonitor.Mapping/Applications/Mappings/ApplicationMapping.cs
+++ OpenVPNGateMonitor.Mapping/Applications/Mappings/ApplicationMapping.cs
@@ -1,5 +1,6 @@
 ﻿using Mapster;
 using OpenVPNGateMonitor.Models;
+using OpenVPNGateMonitor.SharedModels.DataGateMonitorBackend.Applications.Dto;
 using OpenVPNGateMonitor.SharedModels.DataGateMonitorBackend.Applications.Requests;
 using OpenVPNGateMonitor.SharedModels.DataGateMonitorBackend.Applications.Responses;
 
@@ -9,7 +10,22 @@ public class ApplicationMapping : IRegister
 {
     public void Register(TypeAdapterConfig config)
     {
-        config.NewConfig<ClientApplication, ApplicationResponse>();
-        config.NewConfig<RegisterApplicationRequest, ClientApplication>();
+        #region "get-all"
+        config.NewConfig<ClientApplication, ApplicationDto>()
+            .Map(d => d.ClientId, s => s.ClientId)
+            .Map(d => d.Name, s => s.Name);
+        // .Map(d => d.IsRevoked, s => s.IsRevoked)
+        // .Map(d => d.IsSystem, s => s.IsSystem);
+
+        config.NewConfig<List<ClientApplication>, ApplicationsResponse>()
+            .Map(d => d.Application, s => s);
+        #endregion
+
+        #region "register"
+        config.NewConfig<ClientApplication, RegisterApplicationResponse>()
+            .Map(d => d.Name, s => s.Name)
+            .Map(d => d.ClientId, s => s.ClientId)
+            .Map(d => d.ClientSecret, s => s.ClientSecret);
+        #endregion
     }
 }
