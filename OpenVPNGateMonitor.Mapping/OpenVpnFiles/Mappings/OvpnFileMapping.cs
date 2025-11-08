81b2ab1b4544e8d7ba16d32a79ce8ed9bb996739
Ivan <imkolganov@gmail.com>
2025-11-08 21:38:25 +0200

    Adds mapping for single/multiple OvpnFiles
    Adds new AutoMapper configurations to handle the mapping of single and lists of `OvpnFile` and `OvpnFileToken`
    to `OvpnFileWithTokenResponse` and `OvpnFilesWithTokensResponse` respectively.
    This enhancement provides more flexibility for mapping these entity types to their corresponding DTOs.
    Also increments the assembly version.

--- OpenVPNGateMonitor.Mapping/OpenVpnFiles/Mappings/OvpnFileMapping.cs
+++ OpenVPNGateMonitor.Mapping/OpenVpnFiles/Mappings/OvpnFileMapping.cs
@@ -29,5 +29,19 @@ public class OvpnFileMapping : IRegister
         config.NewConfig<(List<IssuedOvpnFile> files, List<IssuedOvpnFileToken> tokens), OvpnFilesWithTokensResponse>()
             .Map(dest => dest.IssuedOvpnFiles, src => src.files)
             .Map(dest => dest.IssuedOvpnFileTokens, src => src.tokens);
+        
+        config.NewConfig<(IssuedOvpnFile File, IssuedOvpnFileToken? Token), OvpnFileWithTokenResponse>()
+            .Map(d => d.IssuedOvpnFile,      s => s.File)
+            .Map(d => d.IssuedOvpnFileToken, s => s.Token ?? new IssuedOvpnFileToken());
+
+        config.NewConfig<List<(IssuedOvpnFile File, IssuedOvpnFileToken? Token)>, OvpnFilesWithTokensResponse>()
+            .Map(d => d.IssuedOvpnFiles,      s => s.Select(x => x.File))
+            .Map(d => d.IssuedOvpnFileTokens, s => s.Where(x => x.Token != null).Select(x => x.Token!));
+        
+        config.NewConfig<List<(IssuedOvpnFile File, IssuedOvpnFileToken? Token)>, OvpnFilesWithTokensResponse>()
+            .Map(d => d.IssuedOvpnFiles, 
+                s => s.Select(p => p.File)) // will use IssuedOvpnFile -> IssuedOvpnFileDto
+            .Map(d => d.IssuedOvpnFileTokens, 
+                s => s.Where(p => p.Token != null).Select(p => p.Token!)); // null-safe
     }
 }
