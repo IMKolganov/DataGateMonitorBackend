a52f39dd6b10e3fafe4bd756f2cb8d5473f86420
Ivan <imkolganov@gmail.com>
2025-11-26 23:26:10 +0200

    Implements message log pagination
    Adds pagination support for message log retrieval.
    
    This change introduces `Page` and `PageSize` properties to the request models for retrieving messages, enabling efficient retrieval of large datasets. It also updates the response models to use a `PagedResponse` object, providing metadata about the current page and total count.

--- OpenVPNGateMonitor.SharedModels/DataGateMonitorBackend/TelegramBotIncomingMessageLog/Requests/GetAllMessagesRequest.cs
+++ OpenVPNGateMonitor.SharedModels/DataGateMonitorBackend/TelegramBotIncomingMessageLog/Requests/GetAllMessagesRequest.cs
@@ -3,13 +3,8 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace OpenVPNGateMonitor.SharedModels.DataGateMonitorBackend.TelegramBotIncomingMessageLog.Requests;
 
-public class GetAllByTelegramIdMessagesRequest
+public class GetAllMessagesRequest
 {
-    [Required(ErrorMessage = "telegramId is required.")]
-    [Range(1, long.MaxValue, ErrorMessage = "telegramId must be greater than 0.")]
-    [FromRoute(Name = "telegramId")]
-    public long TelegramId { get; set; }
-    
     [Range(1, int.MaxValue, ErrorMessage = "page must be greater than 0.")]
     public int Page { get; set; } = 1;
 
