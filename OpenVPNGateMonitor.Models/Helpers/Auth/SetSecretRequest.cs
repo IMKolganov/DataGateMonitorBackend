0160d1c92655e616b9d1d9868c5eec6ddd4d519d
Ivan <imkolganov@gmail.com>
2025-11-27 21:34:45 +0200

    Adds models for authentication requests
    Adds the `SetSecretRequest` and `TokenRequest` classes.
    
    These models are currently marked with a "//todo" comment
    indicating a plan to move them to a shared models NuGet package
    in the future.

--- OpenVPNGateMonitor.Models/Helpers/Auth/SetSecretRequest.cs
+++ OpenVPNGateMonitor.Models/Helpers/Auth/SetSecretRequest.cs
@@ -1,6 +1,6 @@
 ﻿namespace OpenVPNGateMonitor.Models.Helpers.Auth;
 
-public class SetSecretRequest
+public class SetSecretRequest//todo: move to shared models nuget
 {
     public string ClientId { get; set; } = string.Empty;
     public string ClientSecret { get; set; } = string.Empty;
