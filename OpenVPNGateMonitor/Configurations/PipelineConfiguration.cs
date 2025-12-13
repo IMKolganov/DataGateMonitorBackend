0cdcbc2d55e4bb234dbff81bfafde610c25bef5a
Ivan <imkolganov@gmail.com>
2025-12-14 01:36:17 +0200

    Remove websocket method & added signalR method with the same logic

--- OpenVPNGateMonitor/Configurations/PipelineConfiguration.cs
+++ OpenVPNGateMonitor/Configurations/PipelineConfiguration.cs
@@ -101,7 +101,8 @@ public static class PipelineConfiguration
 
         app.MapHub<GeoLiteHub>("/api/hubs/geoLite");
         app.MapHub<OpenVpnFrontendHub>("/api/hubs/frontend");
-        app.MapHub<AdminNotificationHub>("/hubs/admin-notify");
+        app.MapHub<AdminNotificationHub>("/api/hubs/admin-notify");
+        app.MapHub<OpenVpnStatusHub>("/api/status-stream");
 
         var version = Assembly.GetExecutingAssembly().GetName().Version?.ToString() ?? "Unknown version";
         var environmentName = app.Environment.EnvironmentName;
