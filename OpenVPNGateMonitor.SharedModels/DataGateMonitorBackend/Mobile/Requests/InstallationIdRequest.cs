de38a205c5a139ef1b920a12563279a28a250262
Ivan <imkolganov@gmail.com>
2025-12-17 15:43:09 +0200

    Adds device registration to the database
    Implements device registration by adding a new Device model
    and related database configuration.
    
    This allows tracking user devices via unique installation IDs
    and their associated user IDs.

---
+++ OpenVPNGateMonitor.SharedModels/DataGateMonitorBackend/Mobile/Requests/InstallationIdRequest.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OpenVPNGateMonitor.SharedModels.DataGateMonitorBackend.Mobile.Requests;
+
+public class InstallationIdRequest
+{
+    [Required]
+    public string InstallationId { get; set; } = string.Empty;
+}
