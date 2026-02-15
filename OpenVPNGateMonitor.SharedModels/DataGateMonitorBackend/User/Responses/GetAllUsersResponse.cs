ebb7cc6e73a92177ef1b91d02722384e2b336971
Ivan <imkolganov@gmail.com>
2026-02-15 13:19:03 +0200

    Implements paged user retrieval
    Adds pagination support to the user retrieval endpoint. This allows
    fetching users in smaller chunks, improving performance and
    scalability, especially when dealing with large user bases.
    
    Introduces GetAllUsersRequest for specifying page number and size.
    Updates GetAllUsersResponse to include pagination metadata.

--- OpenVPNGateMonitor.SharedModels/DataGateMonitorBackend/User/Responses/GetAllUsersResponse.cs
+++ OpenVPNGateMonitor.SharedModels/DataGateMonitorBackend/User/Responses/GetAllUsersResponse.cs
@@ -1,8 +1,11 @@
-﻿using OpenVPNGateMonitor.SharedModels.DataGateMonitorBackend.User.Responses.Dto;
+using OpenVPNGateMonitor.SharedModels.DataGateMonitorBackend.User.Responses.Dto;
 
 namespace OpenVPNGateMonitor.SharedModels.DataGateMonitorBackend.User.Responses;
 
 public class GetAllUsersResponse
 {
+    public int Page { get; set; }
+    public int PageSize { get; set; }
+    public int TotalCount { get; set; }
     public List<UserDto> Users { get; set; } = new();
 }
