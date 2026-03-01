ad86d421931c2eb68b691207dc2ef9c532db7b32
Ivan <imkolganov@gmail.com>
2026-03-02 01:16:03 +0200

    added test for configurations

---
+++ OpenVPNGateMonitor.Tests/Configurations/JwtConfigurationTests.cs
@@ -0,0 +1,41 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using OpenVPNGateMonitor.Configurations;
+using OpenVPNGateMonitor.Services.Api.Auth.Registers.Interfaces;
+using Xunit;
+
+namespace OpenVPNGateMonitor.Tests.Configurations;
+
+public class JwtConfigurationTests
+{
+    [Fact]
+    public void ConfigureJwt_Registers_AuthenticationAndMicroserviceTokenService()
+    {
+        var services = new ServiceCollection();
+        var config = new ConfigurationBuilder()
+            .AddInMemoryCollection(new Dictionary<string, string?> { ["Jwt:Secret"] = "min-32-char-secret-for-testing!!!!" })
+            .Build();
+
+        services.ConfigureJwt(config);
+
+        AssertRegistered(services, typeof(IMicroserviceTokenService));
+        Assert.Contains(services, d =>
+            d.ServiceType == typeof(Microsoft.AspNetCore.Authentication.IAuthenticationService) ||
+            d.ServiceType == typeof(Microsoft.AspNetCore.Authentication.IAuthenticationHandlerProvider));
+    }
+
+    [Fact]
+    public void ConfigureJwt_WhenJwtSecretMissing_Throws()
+    {
+        var services = new ServiceCollection();
+        var config = new ConfigurationBuilder().Build();
+
+        Assert.Throws<InvalidOperationException>(() => services.ConfigureJwt(config));
+    }
+
+    private static void AssertRegistered(IServiceCollection services, Type serviceType)
+    {
+        var descriptor = services.FirstOrDefault(d => d.ServiceType == serviceType);
+        Assert.True(descriptor != null, $"Expected service {serviceType.Name} to be registered.");
+    }
+}
