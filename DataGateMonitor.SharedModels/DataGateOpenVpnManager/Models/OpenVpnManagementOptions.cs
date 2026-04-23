136f708a8cf3d39fa5a55bdbbf4836691ea12bfe
Ivan <imkolganov@gmail.com>
2026-04-23 20:57:39 +0300

    chore: SharedModels 1.0.6, merge XRay/OpenVPN contracts, bump API 1.0.3.18
    Publish DataGateMonitor.SharedModels 1.0.6 to NuGet before consumers restore.
    Consolidate XRay DTOs and microservice options into the main SharedModels package.
    Refresh OpenAPI swagger schema.

---
+++ DataGateMonitor.SharedModels/DataGateOpenVpnManager/Models/OpenVpnManagementOptions.cs
@@ -0,0 +1,7 @@
+namespace DataGateMonitor.SharedModels.DataGateOpenVpnManager.Models;
+
+public class OpenVpnManagementOptions
+{
+    public string Host { get; set; } = "localhost";
+    public int Port { get; set; } = 5093;
+}
