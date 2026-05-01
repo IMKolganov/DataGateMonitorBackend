518c7473f92f9b00aeca04e6a54f80bb6b604062
Ivan <imkolganov@gmail.com>
2026-05-01 15:36:14 +0300

    feat: add Windows crash ingest API, storage, and diagnostics endpoint

--- DataGateMonitor.DataBase/Migrations/20260501122622_WindowsCrashReports_Init.Designer.cs
+++ DataGateMonitor.DataBase/Migrations/20260501122622_WindowsCrashReports_Init.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using DataGateMonitor.DataBase.Contexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace DataGateMonitor.DataBase.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260501122622_WindowsCrashReports_Init")]
+    partial class WindowsCrashReports_Init
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
