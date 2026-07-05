3bdc765844479c92933719f5bc858d610d54fd59
Ivan <imkolganov@gmail.com>
2026-07-05 20:49:33 +0300

    Stabilize crash ingest and Serilog configuration tests.
    Replace flaky WebApplicationFactory integration tests with controller unit tests and isolate in-memory DB config per factory via IConfiguration instead of process-wide env vars.

--- DataGateMonitor.Tests/Configurations/SerilogConfigurationTests.cs
+++ DataGateMonitor.Tests/Configurations/SerilogConfigurationTests.cs
@@ -1,18 +1,29 @@
 using Microsoft.Extensions.Hosting;
 using DataGateMonitor.Configurations;
+using Serilog;
 using Xunit;
 
 namespace DataGateMonitor.Tests.Configurations;
 
+[CollectionDefinition(nameof(SerilogConfigurationCollection), DisableParallelization = true)]
+public sealed class SerilogConfigurationCollection;
+
+[Collection(nameof(SerilogConfigurationCollection))]
 public class SerilogConfigurationTests
 {
     [Fact]
     public void ConfigureSerilog_DoesNotThrow()
     {
-        var hostBuilder = Host.CreateDefaultBuilder();
-
-        var exception = Record.Exception(() => hostBuilder.ConfigureSerilog());
-
-        Assert.Null(exception);
+        var previous = Log.Logger;
+        try
+        {
+            var hostBuilder = Host.CreateDefaultBuilder();
+            var exception = Record.Exception(() => hostBuilder.ConfigureSerilog());
+            Assert.Null(exception);
+        }
+        finally
+        {
+            Log.Logger = previous;
+        }
     }
 }
